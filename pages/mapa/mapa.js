9df9700b68b4ddd2f0e478b1c2f1cbf3734c987f
jmhurtadoc <jmhurtadoc@unal.edu.co>
2017-10-03 01:23:26 -0500

    mapa add maker

--- pages/mapa/mapa.js
+++ pages/mapa/mapa.js
@@ -16,14 +16,39 @@ function mapaController() {
 
 			var map;
 			$scope.showMap = function () {
+					var myLatLng = {lat: 4.688475, lng: 1-74.117780};
 					var mapdiv = document.getElementById('map');
 					mapdiv.setAttribute('height', '75%');
 					map = new google.maps.Map(mapdiv, {
-						center: {lat: -34.397, lng: 150.644},
+						center: {lat: 4.688475, lng: -74.117780},
 						zoom: 8
 					});
+					
+					// This event listener calls addMarker() when the map is clicked.
+					  google.maps.event.addListener(map, 'click', function(event) {
+					    addMarker(event.latLng, map);
+					  });
+					
+					var marker = new google.maps.Marker({
+				          map: map,
+				          position: myLatLng,
+				          title: 'Hello World!'
+				        });
+					
 					$timeout(function(){document.getElementById('map').setAttribute('height', '80%');}, 100);
 			};
+			
+			function addMarker(location, map) {
+				  // Add the marker at the clicked location, and add the next-available label
+				  // from the array of alphabetical characters.
+				  var marker = new google.maps.Marker({
+				    position: location,
+				    map: map
+				  });
+				  console.log(marker.position	)
+				}
+			
+			
 
 			$scope.showMap();
 
