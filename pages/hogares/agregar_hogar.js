383ea655d602369e49dd8e76edebeee893064729
jmhurtadoc <jmhurtadoc@unal.edu.co>
2017-10-28 01:12:16 -0500

    Se agrega ubicacion hogar por mapa

--- pages/hogares/agregar_hogar.js
+++ pages/hogares/agregar_hogar.js
@@ -10,7 +10,7 @@ function agregarhogarController() {
  
     templateUrl : 'pages/hogares/agregarhogar.htm', 
  
-    controller : [ '$scope', '$state', '$stateParams','$http','variableCliente', function($scope, $state, $stateParams,$http,variableCliente) { 
+    controller : [ '$scope', '$state', '$stateParams','$http','$timeout','variableCliente', function($scope, $state, $stateParams,$http,$timeout,variableCliente) { 
  
       $scope.consultarHogares=function(){
     	  console.log("hola");
@@ -41,10 +41,13 @@ function agregarhogarController() {
       } 
  
       $scope.guardarHogar = function () { 
-        
+        if (markers.length < 1) {
+        	alert("Agrege una ubicación")
+        	return
+		}
         var cliente=variableCliente.getVarCliente(); 
         $scope.hogar["ht_clente_id"] =cliente.id;
-        
+        $scope.hogar.ubicacion = posicionMarker ;
         $http({ 
           method : 'POST', 
           url : SERVER_ENDPOINT + '/hogar/crearHogarCliente', 
@@ -54,6 +57,8 @@ function agregarhogarController() {
         	$scope.ocultarAgregarHogar = true;
         	$scope.hogar.direccion="";
         	$scope.hogar.ubicacion="";
+        	setMapOnAll(null);
+		    markers = [];
         }, function(error) { 
           
         }); 
@@ -76,6 +81,54 @@ function agregarhogarController() {
  
       $scope.greeting = "Este es el clientes"; 
       
- 
+      
+//      -------------------------------------------------------------------------------------
+      	var map;
+      	var markers = [];
+      	var posicionMarker;
+		$scope.showMap = function () {
+				var myLatLng = {lat: 4.688475, lng: 1-74.117780};
+				var mapdiv = document.getElementById('map');
+				mapdiv.setAttribute('height', '75%');
+				map = new google.maps.Map(mapdiv, {
+					center: {lat: 4.688475, lng: -74.117780},
+					zoom: 12
+				});
+//				 This event listener calls addMarker() when the map is clicked.
+				  google.maps.event.addListener(map, 'click', function(event) {
+					setMapOnAll(null);
+				    markers = [];
+				    addMarker(event.latLng, map);
+				  });
+				  $scope.consultarHogares()
+				$timeout(function(){document.getElementById('map').setAttribute('height', '80%');}, 100);
+		};
+		
+		// Sets the map on all markers in the array.
+	      function setMapOnAll(map) {
+	        for (var i = 0; i < markers.length; i++) {
+	          markers[i].setMap(map);
+	        }
+	      }
+		
+		
+		function addMarker(location, map) {
+			  // Add the marker at the clicked location, and add the next-available label
+			  // from the array of alphabetical characters.
+			  var marker = new google.maps.Marker({
+			    position: location,
+			    map: map
+			  });
+			  var latitud = marker.getPosition().lat()
+			  var longitud = marker.getPosition().lng()
+			  posicionMarker = "{\"lat\":"+latitud+", \"lng\": "+longitud+"}" ;
+			  console.log(posicionMarker)
+			  markers.push(marker);
+			}
+		$scope.showMap();
+
+//      -------------------------------------------------------------------------------------
+      
+      
     }]}; 
 };
